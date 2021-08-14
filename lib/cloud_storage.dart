c479b0e02ffc2cdd7f3507df0b8a53bb310f2912
sengtan <tansenghooi1998@gmail.com>
2021-08-14 19:42:56 +0800

    [Firebase Cloud Storage] - Updated & tested taskBox, added retry and cancel buttons.
    [Next Step]
    - Create bootstrap page to update to latest user data / check and download all required user photos

--- lib/cloud_storage.dart
+++ lib/cloud_storage.dart
@@ -1,10 +1,10 @@
-import 'dart:io';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:http/http.dart' as http;
-import 'package:flutter/foundation.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'dart:async';
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -34,7 +34,7 @@ class CloudStorage with ChangeNotifier{
     final String ext = file.path.split('.').last;   //get extension of file
 
     _isUploading = true;
-    _uploadParam.remove(filename);       //reset
+    _uploadParam.remove(filename);      //reset
     _uploadException.remove(filename);  //reset
     notifyListeners();
 
@@ -42,7 +42,7 @@ class CloudStorage with ChangeNotifier{
     final UploadTask uploadTask = _cloudStorage.ref().child("$firebasePath").putFile( //eg. uid123/photos/testing
         file,
         SettableMetadata(
-          contentType: 'image/$ext',  //eg. image/jpg
+          contentType: 'image/$ext',    //eg. image/jpg
         )
     );
     _uploadMap[filename] = uploadTask;  //eg. {"testing":uploadTask}
@@ -75,7 +75,7 @@ class CloudStorage with ChangeNotifier{
     _downloadMap[filename] = downloadTask;
     notifyListeners();
 
-    downloadTask.then((_) => throw(Exception("lol")))
+    downloadTask//.then((_) => throw(Exception("lol")))
       .catchError((Object e){
       _downloadParam[filename] = {"firebasePath":firebasePath};  //to retry if needed
       _downloadException[filename] = e;       //to indicate failure and to check exception
@@ -87,6 +87,30 @@ class CloudStorage with ChangeNotifier{
       notifyListeners();
     });
   }
+
+  void cancelUpload(String filename){
+    if(_uploadMap.containsKey(filename)){
+      if(_uploadParam.containsKey(filename))
+        _uploadParam.remove(filename);
+      if(_uploadException.containsKey(filename))
+        _uploadException.remove(filename);
+      _uploadMap.remove(filename);
+      if(_uploadMap.isEmpty){_isUploading = false;}
+      notifyListeners();
+    }
+  }
+
+  void cancelDownload(String filename){
+    if(_downloadMap.containsKey(filename)){
+      if(_downloadParam.containsKey(filename))
+        _downloadParam.remove(filename);
+      if(_downloadException.containsKey(filename))
+        _downloadException.remove(filename);
+      _downloadMap.remove(filename);
+      if(_downloadMap.isEmpty){_isDownloading = false;}
+      notifyListeners();
+    }
+  }
 }
 
 class taskBox extends ConsumerWidget{
@@ -97,12 +121,14 @@ class taskBox extends ConsumerWidget{
   late CloudStorage _ref;
   double height;
   double width;
+
   taskBox.upload({Key? key, required String taskName, required UploadTask task, required CloudStorage ref, this.height = 30, this.width = 380}): super(key: key) {
     _taskName = taskName;
     _utask = task;
     _ref = ref;
     _mode = "Upload";
   }
+
   taskBox.download({Key? key, required String taskName, required DownloadTask task, required CloudStorage ref, this.height = 30, this.width = 380}): super(key: key) {
     _taskName = taskName;
     _dtask = task;
@@ -110,10 +136,28 @@ class taskBox extends ConsumerWidget{
     _mode = "Download";
   }
 
+  void _uploadImage(){
+    if(_ref.uploadParam.containsKey(_taskName) && _ref.uploadParam[_taskName] != null)
+      _ref.uploadImage(_ref.uploadParam[_taskName]!["firebasePath"],_ref.uploadParam[_taskName]!["file"]);
+  }
 
+  void _downloadImage(){
+    if(_ref.downloadParam.containsKey(_taskName) && _ref.downloadParam[_taskName] != null)
+      _ref.downloadImage(_ref.downloadParam[_taskName]!["firebasePath"]);
+  }
+
+  void _cancelUpload(){
+    _ref.cancelUpload(_taskName);
+  }
+
+  void _cancelDownload(){
+    _ref.cancelDownload(_taskName);
+  }
 
   @override
   Widget build(BuildContext context, ScopedReader watch){
+    final _cloudStorageProvider = ChangeNotifierProvider<CloudStorage>((ref) => _ref);
+    final cloud = watch(_cloudStorageProvider);
     return Container(
       padding: EdgeInsets.fromLTRB(0.2, 0.2, 0.2, 0.2),
       height: height,
@@ -130,100 +174,160 @@ class taskBox extends ConsumerWidget{
             height: maxHeight,
             width: maxWidth,
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Container(
-                  padding: EdgeInsets.fromLTRB(15.0,0.0,2.0,0.0),
+                  padding: EdgeInsets.fromLTRB(5.0,0.0,0.0,0.0),
                   height: maxHeight * 0.95,
-                  width: maxWidth * 0.6,
+                  width: maxWidth * 0.08,
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Icon(
+                      _mode == "Upload"? Icons.file_upload:Icons.file_download,
+                      color: Theme.of(context).colorScheme.surface,
+                    )
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(0.0,0.0,1.0,0.0),
+                  height: maxHeight * 0.95,
+                  width: maxWidth * 0.47,
                   child: Align(
                     alignment: Alignment.centerLeft,
                     child: Text(_taskName, style: Theme.of(context).textTheme.bodyText1, overflow: TextOverflow.ellipsis)
                   ),
                 ),
-                StreamBuilder<TaskSnapshot>(
-                  stream: _utask != null? _utask!.snapshotEvents:_dtask!.snapshotEvents,
-                  builder: (context1, snapshot){
-                    switch(snapshot.connectionState){
-                      case ConnectionState.active:
-                      case ConnectionState.done: {
+                Container(
+                  padding: EdgeInsets.fromLTRB(1.0,0.0,2.0,0.0),
+                  height: maxHeight * 0.95,
+                  width: maxWidth * 0.45,
+                  child: Align(
+                    alignment:  Alignment.centerRight,
+                    child: StreamBuilder<TaskSnapshot>(
+                      stream: _utask != null? _utask!.snapshotEvents:_dtask!.snapshotEvents,
+                      builder: (context1, snapshot){
                         final String progressText;
                         final double progress;
                         progress = snapshot.data != null?((snapshot.data!.bytesTransferred/snapshot.data!.totalBytes)*100).roundToDouble():0;
                         progressText = "${progress.round()}%";
-                        return Row(
-                          children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.all(Radius.circular(20))
-                              ),
-                              padding: EdgeInsets.fromLTRB(2.0, 0.0, 3.0, 0.0),
-                              height: maxHeight * 0.5,
-                              width: maxWidth * 0.2,
-                              child: LinearProgressIndicator(
-                                backgroundColor: Theme.of(context).colorScheme.surface,
-                                valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).colorScheme.onSurface),
-                                value: progress/100,
-                              )
-                            ),
-                            if((_mode == "Upload" && _ref._uploadException.containsKey(_taskName)) || (_mode == "Download" && _ref._downloadException.containsKey(_taskName)))
-                              Container(
-                                  padding: EdgeInsets.fromLTRB(3.0, 0.0, 1.0, 0.0),
-                                  height: maxHeight * 0.95,
-                                  width: maxWidth * 0.10,
-                                  child: Align(
-                                    alignment: Alignment.centerLeft,
-                                      //child: Text(progressText, style: Theme.of(context).textTheme.bodyText2, overflow: TextOverflow.ellipsis)
+                        switch(snapshot.connectionState){
+                          case ConnectionState.waiting:
+                          case ConnectionState.active: {
+                            if(progress != 100){  //before done
+                              return Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  if( !( (_mode == "Upload" && cloud._uploadException.containsKey(_taskName)) || (_mode == "Download" && cloud._downloadException.containsKey(_taskName)) ) ) //if no error
+                                    Align(
+                                      alignment: Alignment.centerRight,
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                                        child: Container(
+                                          padding: EdgeInsets.fromLTRB(0.0, 0.0, 2.0, 0.0),
+                                          height: maxHeight * 0.2,
+                                          width: maxWidth * 0.23,
+                                          child: LinearProgressIndicator(
+                                            backgroundColor: Theme.of(context).colorScheme.surface,
+                                            valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).colorScheme.onSurface),
+                                            value: progress/100,
+                                          ),
+                                        )
+                                      ),
+                                    )
+                                  else  //if error occured
+                                    Container(
+                                      padding: EdgeInsets.fromLTRB(0.0, 0.0, 2.0, 0.0),
+                                      height: maxHeight * 0.5,
+                                      width: maxWidth * 0.23,
+                                      child: Align(
+                                          alignment: Alignment.centerRight,
+                                          child: Text("Failed", style: Theme.of(context).textTheme.bodyText2, overflow: TextOverflow.ellipsis)
+                                      )
+                                    ),
+                                  Container(
+                                    padding: EdgeInsets.fromLTRB(2.0, 0.0, 0.0, 0.0),
+                                    height: maxHeight * 0.95,
+                                    width: maxWidth * 0.2,
                                     child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      crossAxisAlignment: CrossAxisAlignment.center,
                                       children:[
                                         Container(
                                           height: maxHeight*0.95,
-                                          width: maxWidth*0.05,
-                                          child: ElevatedButton(
-                                            onPressed: () => print("LOL"),
-                                            child: Text("Retry", style: Theme.of(context).textTheme.bodyText2, overflow: TextOverflow.ellipsis),
+                                          width: maxWidth*0.095,
+                                          child: IconButton(
+                                            color: Theme.of(context).colorScheme.surface,
+                                            padding: EdgeInsets.fromLTRB(0.0, 0.0, 2.0, 0.0),
+                                            constraints: BoxConstraints(),
+                                            icon: const Icon(Icons.refresh_sharp),
+                                            onPressed: _mode == "Upload"? _uploadImage:_downloadImage,
+                                            tooltip: "Retry",
                                           ),
                                         ),
                                         Container(
                                           height: maxHeight*0.95,
-                                          width: maxWidth*0.05,
-                                          child: ElevatedButton(
-                                            onPressed: () => print("LOL"),
-                                            child: Text("Cancel", style: Theme.of(context).textTheme.bodyText2, overflow: TextOverflow.ellipsis),
+                                          width: maxWidth*0.095,
+                                          child: IconButton(
+                                            color: Theme.of(context).colorScheme.surface,
+                                            padding: EdgeInsets.fromLTRB(2.0, 0.0, 0.0, 0.0),
+                                            constraints: BoxConstraints(),
+                                            icon: const Icon(Icons.cancel_outlined),
+                                            onPressed: _mode == "Upload"? _cancelUpload:_cancelDownload,
+                                            tooltip: "Cancel",
                                           ),
                                         )
                                       ]
                                     )
                                   )
+                                ],
+                              );
+                            }
+                            else{ //done upload/download (most likely wont come in here, since task will be removed once done
+                              return Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Container(
+                                    padding: EdgeInsets.fromLTRB(5.0,0.0,0.0,0.0),
+                                    height: maxHeight * 0.95,
+                                    width: maxWidth * 0.08,
+                                    child: Align(
+                                      alignment: Alignment.centerRight,
+                                      child: Icon(
+                                        _mode == "Upload"? Icons.cloud_done_sharp:Icons.file_download_done,
+                                        color: Theme.of(context).colorScheme.surface,
+                                      )
+                                    ),
+                                  ),
+                                  Container(
+                                    padding: EdgeInsets.fromLTRB(0.0, 0.0, 5.0, 0.0),
+                                    height: maxHeight * 0.95,
+                                    width: maxWidth * 0.12,
+                                    child: Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: Text("Done", style: Theme.of(context).textTheme.bodyText2, overflow: TextOverflow.ellipsis)
+                                    )
+                                  )
+                                ],
+                              );
+                            }
+                          }
+                          case ConnectionState.none:
+                          default: {
+                            return Container(
+                              padding: EdgeInsets.fromLTRB(2.0, 0.0, 2.0, 0.0),
+                              height: maxHeight * 0.95,
+                              width: maxWidth * 0.3,
+                              child: Align(
+                                alignment: Alignment.centerRight,
+                                child: Text("Preparing...", style: Theme.of(context).textTheme.bodyText2, overflow: TextOverflow.ellipsis)
                               )
-                            else
-                              Container(
-                                padding: EdgeInsets.fromLTRB(3.0, 0.0, 1.0, 0.0),
-                                height: maxHeight * 0.95,
-                                width: maxWidth * 0.10,
-                                child: Align(
-                                  alignment: Alignment.centerLeft,
-                                  child: Text(progressText, style: Theme.of(context).textTheme.bodyText2, overflow: TextOverflow.ellipsis)
-                                )
-                              )
-                          ],
-                        );
+                            );
+                          }
+                        }
                       }
-                      case ConnectionState.none:
-                      default: {
-                        return Container(
-                          padding: EdgeInsets.fromLTRB(2.0, 0.0, 2.0, 0.0),
-                          height: maxHeight * 0.95,
-                          width: maxWidth * 0.3,
-                          child: Align(
-                            alignment: Alignment.centerRight,
-                            child: Text("Preparing...", style: Theme.of(context).textTheme.bodyText2, overflow: TextOverflow.ellipsis)
-                          )
-                        );
-                      }
-                    }
-                  }
+                    ),
+                  ),
                 )
               ],
             )
